967d59e55c48fb81e865757309989125cd9ec837
Grishin D.A. <grigoriidan@gmail.com>
2025-10-24 08:32:09 +0000

    Отредактровал Переменые с ключами и удалением лишней ВМ(vms.tf)

--- variables.tf
+++ variables.tf
@@ -5,11 +5,11 @@ variable "flow" {
 
 variable "cloud_id" {
   type    = string
-  default = "b1gn3ndpua1j6jaabf79"
+  default = "b1gsk2bfdl9e6h6ilouc"
 }
 variable "folder_id" {
   type    = string
-  default = "b1gfu61oc15cb99nqmfe"
+  default = "b1g0nbbtab0kcgkrf9qu"
 }
 
 variable "test" {
